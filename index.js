fe3ae9f984001b4e58c81c7cf0606ff163c40be9
Greg Allen <greg@firstandthird.com>
2019-09-16 11:34:02 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = function (arc, cloudformation, stage) {
+  const config = arc.slack || [];
+
+  const url = config[0][1]; //TODO: make this better
+  const channel = config[1][1];
+
+  cloudformation.Resources.lambdaSlackHandler = {
+    Type: 'AWS::Serverless::Application',
+    Properties: {
+      Location: {
+        ApplicationId: 'arn:aws:serverlessrepo:us-east-1:488791064862:applications/lambda-slack-handler',
+        SemanticVersion: '1.0.0'
+      },
+      Parameters: {
+        SlackChannel: channel,
+        SlackUrl: url
+      }
+    }
+  };
+
+  return cloudformation;
+};
