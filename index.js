03299bc18380f97c3c46d8da07692853515a3687
Greg Allen <greg@firstandthird.com>
2019-09-18 09:09:44 -0700

    set memory to 128

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function (arc, cloudformation, stage) {
       Handler: 'index.handler',
       CodeUri: 's3://lambda-slack-handler/95361e066812645ea1d403c78ced1e86',
       Runtime: 'nodejs10.x',
-      MemorySize: 1152,
+      MemorySize: 128,
       Timeout: 5,
       Environment: {
         Variables: {
