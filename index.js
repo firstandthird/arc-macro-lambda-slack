dd4fc65ceaf959f27641960cbe162eaafed4b30a
orthagonal <orthagonal1@gmail.com>
2019-11-04 17:26:10 +0000

    1.3.1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = function (arc, cloudformation, stage) {
     Type: 'AWS::Serverless::Function',
     Properties: {
       Handler: 'index.handler',
-      CodeUri: 's3://lambda-slack-handler/1.0.zip',
+      CodeUri: 's3://lambda-slack-handler/1.3.1.zip',
       Runtime: 'nodejs10.x',
       MemorySize: 128,
       Timeout: 5,
